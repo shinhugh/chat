ab8e8f31555d725270560f4ba754682c065094dc
Hugh Shin <hughbee135@gmail.com>
2022-08-30 21:17:11 -0700

    Initial set up

---
+++ src/chat/LoginServlet.java
@@ -0,0 +1,20 @@
+package chat;
+
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+
+public class LoginServlet extends HttpServlet {
+  public void doPost(HttpServletRequest request, HttpServletResponse response)
+  throws IOException, ServletException
+  {
+    // TODO
+  }
+
+  public void doDelete(HttpServletRequest request,
+  HttpServletResponse response)
+  throws IOException, ServletException
+  {
+    // TODO
+  }
+}
---
+++ src/chat/UserServlet.java
@@ -0,0 +1,32 @@
+package chat;
+
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+
+public class UserServlet extends HttpServlet {
+  public void doGet(HttpServletRequest request, HttpServletResponse response)
+  throws IOException, ServletException
+  {
+    // TODO
+  }
+
+  public void doPost(HttpServletRequest request, HttpServletResponse response)
+  throws IOException, ServletException
+  {
+    // TODO
+  }
+
+  public void doPut(HttpServletRequest request, HttpServletResponse response)
+  throws IOException, ServletException
+  {
+    // TODO
+  }
+
+  public void doDelete(HttpServletRequest request,
+  HttpServletResponse response)
+  throws IOException, ServletException
+  {
+    // TODO
+  }
+}
