a57d54c274ec1b8ddc42f6e887bc584a4bdebe8f
Hugh Shin <hughbee135@gmail.com>
2022-09-01 01:33:13 -0700

    Add helper class for database routines

--- src/chat/APIUserServlet.java
+++ src/chat/APIUserServlet.java
@@ -12,6 +12,7 @@ public class APIUserServlet extends HttpServlet {
     Connection connection = null;
     PreparedStatement statement = null;
     ResultSet results = null;
+
     try {
       String sessionId = null;
       Cookie[] cookies = request.getCookies();
@@ -32,70 +33,31 @@ public class APIUserServlet extends HttpServlet {
       Class.forName("org.mariadb.jdbc.Driver");
       connection = DriverManager.getConnection
       ("jdbc:mariadb://localhost/chat", "root", "");
-      String queryString = "SELECT user, expiration FROM sessions WHERE id = ?;";
+      int userId = mapSessionIdToUserId(sessionId, connection);
+      if (userId < 0) {
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        return;
+      }
+
+      String queryString = "SELECT name FROM users WHERE id = ?;";
       statement = connection.prepareStatement(queryString);
-      statement.setString(1, sessionId);
+      statement.setString(1, userId);
       results = statement.executeQuery();
-      if (!results.next()) {
-        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-        return;
-      }
-      int sessionExpiration = results.getInt(2);
-      if (sessionExpiration <= System.currentTimeMillis()) {
-        try {
-          results.close();
-        } catch (Exception error) { }
-        try {
-          statement.close();
-        } catch (Exception error) { }
-        queryString = "DELETE FROM sessions WHERE id = ?;";
-        statement = connection.prepareStatement(queryString);
-        statement.setString(1, sessionId);
-        statement.executeUpdate();
-        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-        return;
-      }
-      int userId = results.getInt(1);
-      try {
-        results.close();
-      } catch (Exception error) { }
-      try {
-        statement.close();
-      } catch (Exception error) { }
-      queryString = "SELECT name FROM users WHERE id = ?;";
-      statement = connection.prepareStatement(queryString);
-      statement.setInt(1, userId);
-      results = statement.executeQuery();
-      if (!results.next()) {
-        try {
-          results.close();
-        } catch (Exception error) { }
-        try {
-          statement.close();
-        } catch (Exception error) { }
-        queryString = "DELETE FROM sessions WHERE user = ?;";
-        statement = connection.prepareStatement(queryString);
-        statement.setInt(1, userId);
-        statement.executeUpdate();
-        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-        return;
-      }
       String userName = results.getString(1);
       response.setContentType("application/json");
       PrintWriter out = response.getWriter();
       out.println("{\"name\":\"" + userName + "\"}");
       response.setStatus(HttpServletResponse.SC_OK);
-    } catch (SQLException error) {
+    }
+
+    catch (SQLException error) {
       response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-    } catch (ClassNotFoundException error) {
+    }
+    catch (ClassNotFoundException error) {
       response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-    } finally {
-      try {
-        results.close();
-      } catch (Exception error) { }
-      try {
-        statement.close();
-      } catch (Exception error) { }
+    }
+    finally {
+      close(statement, results);
       try {
         connection.close();
       } catch (Exception error) { }
---
+++ src/chat/DbHelper.java
@@ -0,0 +1,56 @@
+package chat;
+
+import java.sql.*;
+
+public class DbHelper {
+  public static void close(PreparedStatement statement, ResultSet results) {
+    try {
+      results.close();
+    } catch (Exception error) { }
+    try {
+      statement.close();
+    } catch (Exception error) { }
+  }
+
+  public static int mapSessionIdToUserId(String sessionId,
+  Connection connection) throws SQLException {
+    PreparedStatement statement = null;
+    ResultSet results = null;
+    try {
+      String queryString
+      = "SELECT user, expiration FROM sessions WHERE id = ?;";
+      statement = connection.prepareStatement(queryString);
+      statement.setString(1, sessionId);
+      results = statement.executeQuery();
+      if (!results.next()) {
+        return -1;
+      }
+      int sessionExpiration = results.getInt(2);
+      if (sessionExpiration <= System.currentTimeMillis()) {
+        close(statement, results);
+        queryString = "DELETE FROM sessions WHERE id = ?;";
+        statement = connection.prepareStatement(queryString);
+        statement.setString(1, sessionId);
+        statement.executeUpdate();
+        return -1;
+      }
+      int userId = results.getInt(1);
+      close(statement, results);
+      queryString = "SELECT * FROM users WHERE id = ?;";
+      statement = connection.prepareStatement(queryString);
+      statement.setInt(1, userId);
+      results = statement.executeQuery();
+      if (!results.next()) {
+        close(statement, results);
+        queryString = "DELETE FROM sessions WHERE user = ?;";
+        statement = connection.prepareStatement(queryString);
+        statement.setInt(1, userId);
+        statement.executeUpdate();
+        return -1;
+      }
+      return userId;
+    } finally {
+      close(statement, results);
+    }
+  }
+}
