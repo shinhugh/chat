6a70aa0d69df55316c2ee541afe022085b7fef76
Hugh Shin <hughbee135@gmail.com>
2022-09-02 18:13:35 -0700

    Add socket gating via session cookie

--- src/chat/SocketMessageConfigurator.java
+++ src/chat/SocketMessageConfigurator.java
@@ -1,6 +1,7 @@
 package chat;
 
 import java.util.*;
+import java.sql.*;
 import jakarta.websocket.*;
 import jakarta.websocket.server.*;
 
@@ -9,10 +10,45 @@ extends ServerEndpointConfig.Configurator {
   @Override
   public void modifyHandshake(ServerEndpointConfig config,
   HandshakeRequest request, HandshakeResponse response) {
+    super.modifyHandshake(config, request, response);
     List<String> cookies = request.getHeaders().get("cookie");
-    // TODO: Parse cookies for session token
-    String session = "MOCKSESSION";
-    // TODO: Reject connection if session is missing or invalid
+    String session = null;
+    for (String cookie : cookies) {
+      if (cookie.startsWith("session") && cookie.length() > 8) {
+        session = cookie.substring(8);
+      }
+    }
+    if (session == null) {
+      response.getHeaders()
+      .put(HandshakeResponse.SEC_WEBSOCKET_ACCEPT, new ArrayList<String>());
+      return;
+    }
+
+    Connection connection = null;
+    PreparedStatement statement = null;
+    ResultSet results = null;
+    try {
+      Class.forName("org.mariadb.jdbc.Driver");
+      connection = DriverManager.getConnection
+      ("jdbc:mariadb://localhost/chat", "root", "");
+      String queryString = "SELECT * FROM sessions WHERE id = ?;";
+      statement = connection.prepareStatement(queryString);
+      statement.setString(1, session);
+      results = statement.executeQuery();
+      if (!results.next()) {
+        response.getHeaders()
+        .put(HandshakeResponse.SEC_WEBSOCKET_ACCEPT, new ArrayList<String>());
+        return;
+      }
+    } catch (Exception error) {
+      response.getHeaders()
+      .put(HandshakeResponse.SEC_WEBSOCKET_ACCEPT, new ArrayList<String>());
+      return;
+    } finally {
+      DbHelper.close(statement, results);
+      DbHelper.close(connection);
+    }
+
     config.getUserProperties().put("session", session);
   }
 }
