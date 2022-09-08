cf8ba24d272ea31eafe96c2651675008fd1fbd63
Hugh Shin <hughbee135@gmail.com>
2022-09-07 18:04:55 -0700

    Progress

--- src/chat/PersistentModel.java
+++ src/chat/PersistentModel.java
@@ -5,6 +5,7 @@ package chat;
 
 import java.lang.*;
 import java.sql.*;
+import java.util.*;
 
 class PersistentModel {
   public static PersistentModel shared = new PersistentModel();
@@ -356,32 +357,180 @@ class PersistentModel {
 
   public Session getSessionById(String sessionId)
   throws Exception {
-    return null;
-    // TODO
+    if (Utilities.nullOrEmpty(sessionId)) {
+      throw new IllegalArgumentException("Illegal argument");
+    }
+
+    if (!assureConnection()) {
+      throw new Exception("Cannot establish connection with database");
+    }
+
+    PreparedStatement statement = null;
+    ResultSet results = null;
+
+    try {
+      String queryString = "SELECT user, expiration FROM sessions"
+      + " WHERE id = ?;";
+      statement = connection.prepareStatement(queryString);
+      statement.setString(1, sessionId);
+      results = statement.executeQuery();
+      if (!results.next()) {
+        return null;
+      }
+
+      Session session = new Session();
+      session.id = sessionId;
+      session.user = results.getInt(1);
+      session.expiration = results.getLong(2);
+      return session;
+    }
+
+    catch (SQLException error) {
+      throw new Exception("Encountered unexpected behavior with database");
+    }
+    finally {
+      close(results);
+      close(statement);
+    }
   }
 
   public boolean createSession(Session session)
   throws Exception {
-    return false;
-    // TODO
+    if (session == null || Utilities.nullOrEmpty(session.id)
+    || session.user < 1 || session.expiration < 0) {
+      throw new IllegalArgumentException("Illegal argument");
+    }
+
+    if (!assureConnection()) {
+      throw new Exception("Cannot establish connection with database");
+    }
+
+    PreparedStatement statement = null;
+    ResultSet results = null;
+
+    try {
+      String queryString = "SELECT * FROM sessions WHERE id = ?;";
+      statement = connection.prepareStatement(queryString);
+      statement.setString(1, session.id);
+      results = statement.executeQuery();
+      if (results.next()) {
+        return false;
+      }
+
+      close(results);
+      close(statement);
+      queryString = "INSERT INTO sessions (id, user, expiration)"
+      + " VALUES (?, ?, ?);";
+      statement = connection.prepareStatement(queryString);
+      statement.setString(1, session.id);
+      statement.setInt(2, session.user);
+      statement.setLong(3, session.expiration);
+      return statement.executeUpdate() == 1;
+    }
+
+    catch (SQLException error) {
+      throw new Exception("Encountered unexpected behavior with database");
+    }
+    finally {
+      close(results);
+      close(statement);
+    }
   }
 
   public boolean deleteSessionById(String sessionId)
   throws Exception {
-    return false;
-    // TODO
+    if (Utilities.nullOrEmpty(sessionId)) {
+      throw new IllegalArgumentException("Illegal argument");
+    }
+
+    if (!assureConnection()) {
+      throw new Exception("Cannot establish connection with database");
+    }
+
+    PreparedStatement statement = null;
+
+    try {
+      String queryString = "DELETE FROM sessions WHERE id = ?;";
+      statement = connection.prepareStatement(queryString);
+      statement.setString(1, sessionId);
+      return statement.executeUpdate() == 1;
+    }
+
+    catch (SQLException error) {
+      throw new Exception("Encountered unexpected behavior with database");
+    }
+    finally {
+      close(statement);
+    }
   }
 
   public Message[] getMessages()
   throws Exception {
-    return null;
-    // TODO
+    if (!assureConnection()) {
+      throw new Exception("Cannot establish connection with database");
+    }
+
+    PreparedStatement statement = null;
+    ResultSet results = null;
+
+    try {
+      ArrayList<Message> messages = new ArrayList<Message>();
+      String queryString = "SELECT id, user, timestamp, content FROM messages;";
+      statement = connection.prepareStatement(queryString);
+      results = statement.executeQuery();
+      while (results.next()) {
+        Message message = new Message();
+        message.id = results.getInt(1);
+        message.user = results.getInt(2);
+        message.timestamp = results.getLong(3);
+        message.content = results.getString(4);
+        messages.add(message);
+      }
+      Message[] output = new Message[messages.size()];
+      for (int i = 0; i < messages.size(); i++) {
+        output[i] = messages.get(i);
+      }
+      return output;
+    }
+
+    catch (SQLException error) {
+      throw new Exception("Encountered unexpected behavior with database");
+    }
+    finally {
+      close(results);
+      close(statement);
+    }
   }
 
   public boolean createMessage(Message message)
   throws Exception {
-    return false;
-    // TODO
+    if (message == null || message.user < 1 || message.timestamp < 0
+    || Utilities.nullOrEmpty(message.content)) {
+      throw new IllegalArgumentException("Illegal argument");
+    }
+
+    if (!assureConnection()) {
+      throw new Exception("Cannot establish connection with database");
+    }
+
+    PreparedStatement statement = null;
+
+    try {
+      String queryString = "INSERT INTO messages (user, timestamp, content)"
+      + " VALUES (?, ?, ?);";
+      statement = connection.prepareStatement(queryString);
+      statement.setInt(1, message.user);
+      statement.setLong(2, message.timestamp);
+      statement.setString(3, message.content);
+      return statement.executeUpdate() == 1;
+    }
+
+    catch (SQLException error) {
+      throw new Exception("Encountered unexpected behavior with database");
+    }
+    finally {
+      close(statement);
+    }
   }
 
   private PersistentModel() {
